fae43e641a28176cd2edfb765654cecb7436ad45
Jorge Sisco <jorgesisco17@gmail.com>
2021-01-25 19:53:53 -0400

    Adding animation to the Header button and hero text and hero button

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,9 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Building a better you</h1>
+        <h1>
+          Building a better <br /> you for life!
+        </h1>
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste maxime
           ipsa eaque aliquid rerum ut officiis consectetur nesciunt odit
