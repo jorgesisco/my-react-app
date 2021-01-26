24203ad9538b5cc31a685372b264ef71b76f641b
Jorge Sisco <jorgesisco17@gmail.com>
2021-01-26 15:40:19 -0400

    Created stateful component to set changes to header background when scrolling

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,50 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/">
-        <img src={require('../images/logo-designcode.svg')} width="30"></img>
-      </Link>
-      <Link to="/courses">Our Services</Link>
-      <Link to="/downloads">Case Studies</Link>
-      <Link to="/workshops ">Blog</Link>
-      <Link to="/buy">
-        <button>Book a free consultation</button>
-      </Link>
-    </div>
-  </div>
-)
 
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasScrolled: false,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}
+      >
+        <div className="HeaderGroup">
+          <Link to="/">
+            <img
+              src={require('../images/logo-designcode.svg')}
+              width="30"
+            ></img>
+          </Link>
+          <Link to="/courses">Our Services</Link>
+          <Link to="/downloads">Case Studies</Link>
+          <Link to="/workshops ">Blog</Link>
+          <Link to="/buy">
+            <button>Book a free consultation</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
 export default Header
