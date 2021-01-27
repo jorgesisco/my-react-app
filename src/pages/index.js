e572698202d67b7e169dd58794723d1bf4971d9c
Jorge Sisco <jorgesisco17@gmail.com>
2021-01-26 21:12:22 -0400

    added static data from loca json file

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,29 @@ import Link from 'gatsby-link'
 import Card from '../components/Card';
 import Section from '../components/Section';
 import Wave from '../components/Wave';
+import staticData from '../../staticData.json'
+import Cell from '../components/Cell';
+import styled from 'styled-components'
+
+const SectionCaption = styled.p`
+  font-weight:600;
+  font-size:18px;
+  text-transform: uppercase;
+  color:  #94A4BA;
+  text-align: center;
+`
+
+const SectionCellGroup = styled.div`
+  max-width: 800px;
+  margin: 0 auto 100px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 20px;
+  padding: 0 20px;
+  @media (max-width:800px){
+    grid-template-columns: repeat(1, 1fr);
+  }
+`
 
 const IndexPage = () => (
   <div>
@@ -59,6 +82,15 @@ const IndexPage = () => (
     title="Choose best plan for you"
     text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit cum at quos aut exercitationem porro provident."
     />
+    
+    <SectionCaption>12 sections - 6 hours</SectionCaption>
+     <SectionCellGroup>
+     {staticData.cells.map(cell => (
+      <Cell title={cell.title}
+            image={cell.image}
+      />
+    ))} 
+     </SectionCellGroup>
   </div>
 )
 
