380add1bcc718e7001f9d5663097f540e3fca595
Jorge Sisco <jorgesisco17@gmail.com>
2021-01-27 13:46:02 -0400

    Gatsby build

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 // import Header from './header'
-import './index.css'
+import './Layout.css'
 import Footer from './Footer'
 
 export default function Layout({ children }) {
